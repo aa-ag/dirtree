45cba3fc9d347d262e7d0a640cf465a270f1f75e
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-25 17:42:02 -0600

    deleting entire tree

--- generate.py
+++ generate.py
@@ -1,6 +1,12 @@
 ###--- IMPORTS ---###
 import subprocess
 import csv
+import shutil
+import settings
+
+
+###--- GLOBAL VARIABLES ---###
+path = settings.PATH
 
 
 ###--- FUNCTIONS ---###
@@ -8,9 +14,19 @@ def draw_tree():
     '''
      runs command and saves output/tree in file
     '''
+    # Alternative 1: tree
     subprocess.run('tree > tree.html', shell=True)
+
+
+def delete_tree():
+    '''
+     removes directory specified in path
+    '''
+    global path
+    shutil.rmtree(path, ignore_errors=True)
 
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
-    draw_tree()
+    # draw_tree()
+    delete_tree()
